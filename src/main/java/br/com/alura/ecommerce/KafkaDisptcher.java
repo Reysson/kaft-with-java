c129906f20a8b3308e24b6e34bcd4c25f7e28709
Reysson <reysson.martins30@gmail.com>
2021-08-04 10:49:55 -0400

    Serialização com Gson

--- src/main/java/br/com/alura/ecommerce/KafkaDisptcher.java
+++ src/main/java/br/com/alura/ecommerce/KafkaDisptcher.java
@@ -14,16 +14,16 @@ import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
-public class KafkaDisptcher implements Closeable {
+public class KafkaDisptcher<T> implements Closeable {
 
-    private final KafkaProducer<String,String> producer;
+    private final KafkaProducer<String,T> producer;
 
     public KafkaDisptcher() {
-        this.producer = new KafkaProducer<String,String>(properties());
+        this.producer = new KafkaProducer<>(properties());
     }
 
-    public void send(String topic, String key, String value) throws ExecutionException, InterruptedException {
-        var record = new ProducerRecord<String,String>(topic,key,value);
+    public void send(String topic, String key, T value) throws ExecutionException, InterruptedException {
+        var record = new ProducerRecord<>(topic,key,value);
         Callback callback = (data, ex) -> {
             if (ex != null) {
                 ex.printStackTrace();
@@ -38,7 +38,7 @@ public class KafkaDisptcher implements Closeable {
         var properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GsonSerializer.class.getName());
         return properties;
     }
 
--- src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -6,6 +6,7 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
+import java.math.BigDecimal;
 import java.util.Map;
 import java.util.Properties;
 import java.util.UUID;
@@ -14,17 +15,22 @@ import java.util.concurrent.ExecutionException;
 public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-        try(var dipatcher = new KafkaDisptcher()){
-            for(int i = 1; i<=10;i++){
-                //envio de evento de pedido
-                var key = UUID.randomUUID().toString();
-                var value = key+" 123,321,456";
-                dipatcher.send("ECOMMERCE_NEW_ORDER",key,value);
+        try(var orderDispatcher = new KafkaDisptcher<Order>()){
+            try(var emailDispatcher = new KafkaDisptcher<String>()){
+                for(int i = 1; i<=10;i++){
+                    //envio de evento de pedido
+                    var userId = UUID.randomUUID().toString();
+                    var orderId = UUID.randomUUID().toString();
+                    var amount = new BigDecimal(Math.random() * 5000 + 1);
+                    var order = new Order(userId, orderId, amount);
+                    orderDispatcher.send("ECOMMERCE_NEW_ORDER",userId,order);
 
-                //envio de evento de email
-                var email = "Enviando email 123564 wdhcdbhb" + "";
-                dipatcher.send("ECOMMERCE_SEND_EMAIL",key,email);
+                    //envio de evento de email
+                    var email = "Enviando email 123564 wdhcdbhb" + "";
+                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL",userId,email);
+                }
             }
+
         }
     }
 }
